dd3894c539b61ab9e7ca2fb07eeb47874aba06e2
Felix Springer <felixspringer149@gmail.com>
2018-06-08 08:47:14 +0200

    got libinput

---
+++ .config/i3/scripts/touchpad.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DEVICE="ELAN0501:00 04F3:3019 Touchpad"
+
+xinput set-prop "$DEVICE" 142 1 #disable dis shit (vim op)
+xinput set-prop "$DEVICE" 279 1
+xinput set-prop "$DEVICE" 287 1
