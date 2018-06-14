4c1813b7e4fb941b8dc04d8d555ad20bcd3fcc1f
Felix Springer <felixspringer149@gmail.com>
2018-06-14 13:25:49 +0200

    fix touchpad

--- .config/i3/scripts/touchpad.sh
+++ .config/i3/scripts/touchpad.sh
@@ -2,6 +2,6 @@
 
 DEVICE="ELAN0501:00 04F3:3019 Touchpad"
 
-xinput set-prop "$DEVICE" 142 0 #disable dis shit (vim op)
 xinput set-prop "$DEVICE" 279 1
 xinput set-prop "$DEVICE" 287 1
+xinput set-prop "$DEVICE" 142 0 #disable dis shit (vim op)
