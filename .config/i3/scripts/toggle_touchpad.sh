0678bbb29c9dc97de758b8aaf97045931351a358
Felix Springer <felixspringer149@gmail.com>
2018-06-14 12:51:43 +0200

    touchpad configured

---
+++ .config/i3/scripts/toggle_touchpad.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEVICE="ELAN0501:00 04F3:3019 Touchpad"
+
+STRING=$(xinput list-props "$DEVICE" | grep "Device Enabled (142)")
+STATE=${STRING:$(expr ${#STRING} - 1):1}
+
+NEWSTATE=$(( $(($STATE + 1)) % 2))
+
+xinput set-prop "$DEVICE" 142 $NEWSTATE
