17e31c05f25228c9f7e026e5300fbaf38d5cdfbf
Felix Springer <felixspringer149@gmail.com>
2018-06-17 19:26:42 +0200

    using gpg instead of pass for offlineimap

--- .offlineimap/offlineimap_pass.py
+++ .offlineimap/offlineimap_pass.py
@@ -3,4 +3,4 @@ from subprocess import check_output
 
 
 def get_pass(account):
-    return check_output("pass Mail/" + account, shell=True).splitlines()[0]
+    return check_output("gpg --quiet --for-your-eyes-only --no-tty --decrypt ~/.password-store/Mail/" + account + ".gpg", shell=True).splitlines()[0]
