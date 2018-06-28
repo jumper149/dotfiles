2d610cd715d60475789551ed31de4ebeda64db65
Felix Springer <felixspringer149@gmail.com>
2018-06-28 21:54:08 +0200

    personalize x201t

--- .config/i3blocks/scripts/space.sh
+++ .config/i3blocks/scripts/space.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-HARDDRIVE=/dev/sdb3
-
-#SIZE1=$(df $HARDDRIVE -h --output=size)
-#SIZE2=${SIZE1:6}
-#SIZE3=$(echo $SIZE2 | sed 's/ //g')
-#
-#USED1=$(df $HARDDRIVE -h --output=used)
-#USED2=${USED1:6}
-#USED3=$(echo $USED2 | sed 's/ //g')
-#
-#echo "$USED3/$SIZE3"
+HARDDRIVE=/dev/sda2
 
 FREE1=$(df $HARDDRIVE -h --output=avail)
 FREE2=${FREE1:6}
--- .config/i3blocks/scripts/wifi.sh
+++ .config/i3blocks/scripts/wifi.sh
@@ -2,7 +2,7 @@
 
 # written for ip (which is standard installed)
 
-DEVICE=wlp3s0
+DEVICE=wlp2s0
 INFO=$(ip address show dev $DEVICE | grep inet\ )
 IPADR=${INFO:$(expr index "$INFO" inet + 4)}
 IPADRFORMAT=${IPADR:0:$(expr index "$IPADR" \  - 1)}
