5e351ea57cbcc4875692d856894d2363d1963c01
Felix Springer <felixspringer149@gmail.com>
2018-07-04 13:44:43 +0200

    changed locknotify to use xbacklight

--- .config/i3/scripts/locknotify.sh
+++ .config/i3/scripts/locknotify.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
-value=$(cat /sys/class/backlight/intel_backlight/brightness)
+#value=$(cat /sys/class/backlight/intel_backlight/brightness)
+value=$(xbacklight -get)
 
 slptime=0.25
 lowbri=1
 highbri=99
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+xbacklight -set "$lowbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+xbacklight -set "$highbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+xbacklight -set "$lowbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+xbacklight -set "$highbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$lowbri"
+xbacklight -set "$lowbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$highbri"
+xbacklight -set "$highbri"
 
 sleep $slptime
 
-tee /sys/class/backlight/intel_backlight/brightness <<< "$value"
+#tee /sys/class/backlight/intel_backlight/brightness <<< "$value"
+xbacklight -set "$value"
