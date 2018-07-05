c885d9df1a52226379b6b7e9f51a0b604b70e58f
Felix Springer <felixspringer149@gmail.com>
2018-07-05 16:51:59 +0200

    almost no more transparency on i3bar

--- .config/i3/scripts/mount_known.sh
+++ .config/i3/scripts/mount_known.sh
@@ -3,7 +3,7 @@
 # know arrays are mounted on execution
 
 # add known UUIDs to the array below
-KNOWN_LIST=( "CC78-FDFE" "3AE57D7D33FF9529" )
+KNOWN_LIST=( "CC78-FDFE" "3AE57D7D33FF9529" "0011-9095" )
 
 for UUID in /dev/disk/by-uuid/* ; do
 	for KNOWN_UUID in "${KNOWN_LIST[@]}" ; do
