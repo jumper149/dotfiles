cc62d0b4665462c93879008352880029ff246e4a
Felix Springer <felixspringer149@gmail.com>
2018-07-09 11:25:49 +0200

    made backup safer

--- .config/i3/scripts/usb_backup.sh
+++ .config/i3/scripts/usb_backup.sh
@@ -5,6 +5,18 @@
 DRIVES=$(echo /media/*)
 DRIVES=$(echo "$DRIVES" | sed -e 's@/media/@@g')
 SPOT=$(~/.config/i3/scripts/decide.sh $DRIVES)
+
+# exclude following cases
+case $SPOT in
+	"")
+		exit
+		;;
+	"jumper")
+		echo "dont backup here"
+		exit
+		;;
+esac
+
 SPOT="/media/$SPOT/backup_$HOSTNAME/"
 echo "backup to $SPOT"
 
