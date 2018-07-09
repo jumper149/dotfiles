e63bee59820d33d344e69a3f9697ae868d02a442
Felix Springer <felixspringer149@gmail.com>
2018-07-10 00:02:36 +0200

    added temperature at i3blocks

---
+++ .config/i3blocks/scripts/temperature.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# needs to be adapted for non thinkpads
+# this is for an X201t
+
+INFO=$(cat /proc/acpi/ibm/thermal)
+INFO=${INFO:14}
+TEMP=${INFO:0:$(expr index "$INFO" \  - 1)}
+
+echo "TEMP $TEMP°C"
