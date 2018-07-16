68deebda8b287a5833925af8cd7696a241a07ebd
Felix Springer <felixspringer149@gmail.com>
2018-07-16 22:11:06 +0200

    finished install_core on x220

--- .readme/install_core.sh
+++ .readme/install_core.sh
@@ -1,3 +1,8 @@
 # written for archlinux/pacman
 
-PKGLIST=$()
+CORELIST="$(cat ~/.readme/core | tr '\n' ' ')"
+GROUPSLIST="$(cat ~/.readme/groups | tr '\n' ' ')"
+
+PKGLIST="$GROUPSLIST $CORELIST"
+
+sudo pacman -S $PKGLIST --needed
