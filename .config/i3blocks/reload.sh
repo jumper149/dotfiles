0abb3a82760e5e40e6fe9f39bfd9834e3995672d
Felix Springer <felixspringer149@gmail.com>
2018-08-06 17:51:03 +0200

    reload bar blocks

---
+++ .config/i3blocks/reload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pkill --signal SIGRTMIN+11 i3blocks
+pkill --signal SIGRTMIN+12 i3blocks
+pkill --signal SIGRTMIN+13 i3blocks
+pkill --signal SIGRTMIN+14 i3blocks
+pkill --signal SIGRTMIN+15 i3blocks
+pkill --signal SIGRTMIN+16 i3blocks
+pkill --signal SIGRTMIN+17 i3blocks
+pkill --signal SIGRTMIN+18 i3blocks
+pkill --signal SIGRTMIN+19 i3blocks
+pkill --signal SIGRTMIN+20 i3blocks
+pkill --signal SIGRTMIN+21 i3blocks
+pkill --signal SIGRTMIN+22 i3blocks
