04335eab74ae15cc645e704ed2d27b3598f19caa
Felix Springer <felixspringer149@gmail.com>
2018-08-09 20:37:43 +0200

    pass for irssi

---
+++ .irssi/scripts/pass-freenode.pl
@@ -0,0 +1,11 @@
+#!/bin/perl
+
+my $serv = "freenode";
+my $url = "freenode.net";
+my $user = "jumper149";
+
+my $cmd = "gpg --quiet --for-your-eyes-only --no-tty --decrypt ~/.password-store/$url/$user.gpg";
+my $pass = `$cmd`;
+
+my $server = Irssi::server_find_chatnet($serv);
+$server->command("MSG NickServ IDENTIFY $pass");
