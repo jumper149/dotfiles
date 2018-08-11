045c9d995ea64200a884daec614f5b984508ea14
Felix Springer <felixspringer149@gmail.com>
2018-08-12 00:08:58 +0200

    fancier screenkey

--- .config/i3/scripts/screenkey.sh
+++ .config/i3/scripts/screenkey.sh
@@ -2,4 +2,10 @@
 
 pgrep "screenkey" && killall "screenkey" && exit 0
 
-screenkey -f "Inconsolata"
+XRES=$(cat ~/.Xresources)
+COLOR0=$(echo "$XRES" | grep 'color0:')
+color0=${COLOR0:$(expr index "$COLOR0" \# - 1)}
+COLORe=$(echo "$XRES" | grep 'color15:')
+colore=${COLORe:$(expr index "$COLORe" \# - 1)}
+
+screenkey --no-systray --font "Inconsolata Bold" --bg-color "$color0" --font-color "$colore" --opacity 0.75 --timeout 1.5
