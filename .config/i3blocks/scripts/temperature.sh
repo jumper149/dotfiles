1742a9103d79065e4d9b5758425ac3f613d45df6
Felix Springer <felixspringer149@gmail.com>
2018-08-12 15:26:28 +0200

    better temperature script

--- .config/i3blocks/scripts/temperature.sh
+++ .config/i3blocks/scripts/temperature.sh
@@ -7,13 +7,18 @@ then
 	INFO=$(sensors -A coretemp-isa-0000)
 	INFO=$(echo "$INFO" | grep "Package id 0")
 	TEMP=${INFO:$( expr index "$INFO" "\+"):2}
-	echo "TEMP $TEMP°C"
 elif [ "$(hostname)" = "x201arch" ]
 then
 	INFO=$(cat /proc/acpi/ibm/thermal)
 	INFO=${INFO:14}
 	TEMP=${INFO:0:$(expr index "$INFO" \  - 1)}
-	echo "TEMP $TEMP°C"
 else
 	exit 1
+fi
+
+if [ "$TEMP" = "" ]
+then
+	exit 1
+else
+	echo "TEMP $TEMP°C"
 fi
