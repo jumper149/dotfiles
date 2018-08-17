54e86fff1b2e160053ddbe30043c06642110dd57
Felix Springer <felixspringer149@gmail.com>
2018-08-17 12:17:41 +0200

    better traffic script

--- .config/i3blocks/scripts/traffic.sh
+++ .config/i3blocks/scripts/traffic.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# set desired device
+
+#!/bin/bash
+
+# needs to be adapted
+
+if [ "$(hostname)" = "x220arch" ]
+then
+	DEVICE=wlp2s0
+elif [ "$(hostname)" = "x201arch" ]
+then
+	DEVICE=wlp5s0
+else
+	exit 1
+fi
+
 LENGTH=11
 INFO="$(vnstat --iface wlp2s0 -tr 2 --json | sed 's/,/\n/g')"
 
@@ -6,11 +24,18 @@ RXVAL="$(echo "$INFO" | grep "$lookuprx")"
 RXVAL="${RXVAL:${#lookuprx}}"
 RXVAL="0000${RXVAL:0:$((${#RXVAL} - 1))}"
 RXVAL="${RXVAL:$((${#RXVAL} - $LENGTH))}"
+RXVAL="$(echo "$RXVAL" | sed 's/bit//g')"
 
 lookuptx='"tx":{"ratestring":"'
 TXVAL="$(echo "$INFO" | grep "$lookuptx")"
 TXVAL="${TXVAL:${#lookuptx}}"
 TXVAL="0000${TXVAL:0:$((${#TXVAL} - 1))}"
 TXVAL="${TXVAL:$((${#TXVAL} - $LENGTH))}"
+TXVAL="$(echo "$TXVAL" | sed 's/bit//g')"
 
-echo "$RXVAL - $TXVAL"
+if [ "$RXVAL" = "" ] && [ "$TXVAL" = "" ]
+then
+	exit 2
+else
+	echo "$RXVAL - $TXVAL"
+fi
