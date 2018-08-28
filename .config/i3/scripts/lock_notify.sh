e38391e39e9ceadc4488d8acf9c436bff7ac7acc
Felix Springer <felixspringer149@gmail.com>
2018-08-28 02:46:04 +0200

    lock corner prevention

--- .config/i3/scripts/lock_notify.sh
+++ .config/i3/scripts/lock_notify.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# --- prevent-corner: ---
+# top right
+
+# get screen resolution
+INFO="$(xrandr --listmonitors | sed '2q;d')"
+INFO="$(echo "$INFO" | awk '{print $3}')"
+X_RES="${INFO:0:$(expr index "$INFO" "/" - 1)}"
+Y_RES="${INFO:$(expr index "$INFO" "x" )}"
+Y_RES="${Y_RES:0:$(expr index "$Y_RES" "/" - 1)}"
+
+# get mouse position
+INFO="$(xdotool getmouselocation)"
+X_VAL="$(echo $INFO | awk '{print $1}')"
+X_VAL="${X_VAL:2}"
+Y_VAL="$(echo $INFO | awk '{print $2}')"
+Y_VAL="${Y_VAL:2}"
+echo $X_VAL
+echo $Y_VAL
+
+if [ $(( $X_RES - $X_VAL )) -lt 10 ] && [ $Y_VAL -lt 10 ]
+then
+	echo "aborting lock by pressing Super_L"
+	xdotool key 'Super_L'
+	exit 0
+fi
+exit
+
+# --- flashing: ---
+
 MODULE='intel_backlight'
 MODPATH="/sys/class/backlight/$MODULE"
 
