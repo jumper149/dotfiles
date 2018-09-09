95ba1b5ad64ea7618c692936d0ec60bc9da50ccb
Felix Springer <felixspringer149@gmail.com>
2018-09-09 13:11:04 +0200

    git in install script

--- .readme/install_core.sh
+++ .readme/install_core.sh
@@ -11,6 +11,7 @@ sudo -k  pacman -S $PKGLIST --needed
 
 #-----------------------------------------------------------------------
 
+git -C Packages/pkgaur/ pull
 AURLIST="$(cat ~/.readme/aur)"
 AURVERSIONLIST=""
 
