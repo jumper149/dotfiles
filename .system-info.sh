52e768424062ae8dceac9fd7bea06230d6f00fd0
Felix Springer <felixspringer149@gmail.com>
2018-09-13 20:52:05 +0200

    fixed sink

--- .system-info.sh
+++ .system-info.sh
@@ -19,7 +19,7 @@ elif [ "$(hostname)" = "deskarch" ]
 then
 	MAIN_NETWORKING_DEVICE='enp7s0'
 	MAIN_PARTITION='/dev/sda3'
-	MAIN_PULSEAUDIO_SINK='3'
+	MAIN_PULSEAUDIO_SINK='2'
 	MAIN_PULSEAUDIO_SOURCE='1'
 fi
 
