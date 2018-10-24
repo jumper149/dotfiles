be35e5eb36da79cbfcc212d23a8819d52bc1c132
Felix Springer <felixspringer149@gmail.com>
2018-10-24 15:44:42 +0200

    added old hardrive to usbmount

--- .config/i3/scripts/usb_mount.sh
+++ .config/i3/scripts/usb_mount.sh
@@ -3,7 +3,7 @@
 # know arrays are mounted on execution
 
 # add known UUIDs to the array below
-KNOWN_LIST=( "CC78-FDFE" "3AE57D7D33FF9529" "0011-9095" "10df1b75-8e8f-4773-b079-c3bd32b85c16" )
+KNOWN_LIST=( "CC78-FDFE" "3AE57D7D33FF9529" "0011-9095" "10df1b75-8e8f-4773-b079-c3bd32b85c16" "16DC46E5DC46BF2D" )
 
 for UUID in /dev/disk/by-uuid/* ; do
 	for KNOWN_UUID in "${KNOWN_LIST[@]}" ; do
