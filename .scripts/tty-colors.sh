10608417574bddf3ad3d4712b76711e820aa8c1f
Felix Springer <felixspringer149@gmail.com>
2018-12-15 18:21:33 +0100

    added tty color script

---
+++ .scripts/tty-colors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Xresources="`cat "$HOME/.Xresources"`"
+
+function EscSeqRGB {
+	N="$1"
+	Hex="`printf '%x\n' $N | tr 'a-z' 'A-Z'`"
+	RGB="`echo "$Xresources" | grep "*.color${N}:" | cut --delimiter="#" --fields="2"`"
+	EscSeq="\e]P${Hex}${RGB}"
+	echo -en "$EscSeq"
+}
+
+for i in `seq 0 15`
+do
+	EscSeqRGB "$i"
+done
+
+clear
