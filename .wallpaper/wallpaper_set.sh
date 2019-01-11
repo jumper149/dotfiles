cf7b36521ea4fe319ceee9d53625e5b643062c57
Felix Springer <felixspringer149@gmail.com>
2019-01-11 03:38:35 +0100

    new wallpaper deskarch

--- .wallpaper/wallpaper_set.sh
+++ .wallpaper/wallpaper_set.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-feh --no-fehbg --bg-fill ~/.wallpaper/space/13.jpg
+if [ "`hostname`" = "deskarch" ]; then
+	feh --no-fehbg --bg-fill ~/.wallpaper/adi-0.jpg --bg-fill ~/.wallpaper/adi-1.jpg
+else
+	feh --no-fehbg --bg-fill ~/.wallpaper/space/13.jpg
+fi
