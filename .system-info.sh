bdfa61a605867bdba1ef5f6d79428cd86980c64f
Felix Springer <felixspringer149@gmail.com>
2019-01-24 23:37:07 +0100

    x200 system info

--- .system-info.sh
+++ .system-info.sh
@@ -17,7 +17,7 @@ then
 elif [ "$(hostname)" = "x201arch" ]
 then
 	MAIN_NETWORKING_DEVICE='wlp5s0'
-	MAIN_PARTITION='/dev/sda2'
+	MAIN_PARTITION='/dev/sda3'
 	MAIN_PULSEAUDIO_SINK='0'
 	MAIN_PULSEAUDIO_SOURCE='1'
 elif [ "$(hostname)" = "deskarch" ]
@@ -29,7 +29,7 @@ then
 elif [ "$(hostname)" = "x200arch" ]
 then
 	MAIN_NETWORKING_DEVICE='wlp2s0'
-	MAIN_PARTITION='/dev/sda3'
+	MAIN_PARTITION='/dev/sda2'
 	MAIN_PULSEAUDIO_SINK='0'
 	MAIN_PULSEAUDIO_SOURCE='1'
 fi
