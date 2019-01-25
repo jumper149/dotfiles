1b6528d8611d87add0475a3c6b27af8a514a9761
Felix Springer <felixspringer149@gmail.com>
2019-01-25 20:41:43 +0100

    i3blocks for x200

--- .config/i3blocks/scripts/fan.sh
+++ .config/i3blocks/scripts/fan.sh
@@ -16,9 +16,15 @@ then
 	SPEED="${SPEED:$(( ${#SPEED} - 4 ))}"
 elif [ "$(hostname)" = "deskarch" ]
 then
-	exit 1
-else
 	exit 2
+elif [ "$(hostname)" = "x200arch" ]
+then
+	INFO=$(cat /proc/acpi/ibm/fan | grep "speed" -m 1)
+	SPEED="${INFO:8}"
+	SPEED="0000$SPEED"
+	SPEED="${SPEED:$(( ${#SPEED} - 4 ))}"
+else
+	exit 1
 fi
 
 echo "$SPEED RPM"
