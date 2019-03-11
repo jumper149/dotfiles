870767d9130285f139adf4f6a3d72d30e30bf4b1
Felix Springer <felixspringer149@gmail.com>
2019-03-11 23:01:41 +0100

    screenkey for multiple screens

--- .scripts/screenkey.sh
+++ .scripts/screenkey.sh
@@ -8,4 +8,7 @@ color0=${COLOR0:$(expr index "$COLOR0" \# - 1)}
 COLORe=$(echo "$XRES" | grep 'color15:')
 colore=${COLORe:$(expr index "$COLORe" \# - 1)}
 
-screenkey --no-systray --font "Inconsolata Bold" --bg-color "$color0" --font-color "$colore" --opacity 0.75 --timeout 1.5
+monitors="`xrandr --listactivemonitors | grep "[0-9]\{1,\}:"`"
+selectedMonitor="`~/.scripts/decide/main.sh "$monitors" | cut --delimiter=: --fields=1`"
+
+screenkey --no-systray --font "Inconsolata Bold" --bg-color "$color0" --font-color "$colore" --opacity 0.75 --timeout 1.5 --scr "$selectedMonitor"
