169e8f30e9b7d5411958237c3b13cc49dc8901a3
Felix Springer <felixspringer149@gmail.com>
2019-05-20 20:48:58 +0200

    fixed pacrec copy

--- .scripts/pacrec.sh
+++ .scripts/pacrec.sh
@@ -29,7 +29,7 @@ function compile {
 
         makepkg
         
-        cp $PKG_NAME-$PKG_VER-$PKG_REL-{any,x86_64,i686}.pkg.tar.xz ../../pkgaur/core/
+        cp $PKG_NAME-$PKG_VER-$NEW_REL-{any,x86_64,i686}.pkg.tar.xz ../../pkgaur/core/
 
         cd ..
 }
