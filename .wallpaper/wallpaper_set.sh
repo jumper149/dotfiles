23bbfd8a730b3330c852e56129a683baf633ebb5
Felix Springer <felixspringer149@gmail.com>
2019-06-14 01:06:55 +0200

    gray gradient wallpaper

--- .wallpaper/wallpaper_set.sh
+++ .wallpaper/wallpaper_set.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "`hostname`" = "deskarch" ]; then
-	feh --no-fehbg --bg-fill ~/.wallpaper/adi-0.jpg --bg-fill ~/.wallpaper/adi-1.jpg
-else
-	feh --no-fehbg --bg-fill ~/.wallpaper/space/13.jpg
-fi
+feh --no-fehbg --bg-fill ~/.wallpaper/gradientBlack.png
