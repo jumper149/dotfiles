0d9d5988fa239516a4ccebcd1c35bea9242b3b77
Felix Springer <felixspringer149@gmail.com>
2019-07-04 02:23:13 +0200

    install core is all nice and shiny again

--- .readme/install_core.sh
+++ .readme/install_core.sh
@@ -1,25 +1,67 @@
-# written for archlinux/pacman
+#!/bin/bash
 
-CORELIST="$(cat ~/.readme/core | tr '\n' ' ')"
+CORELIST="$(cat ~/.readme/core | grep -v '^#' | grep -v '^$' | tr '\n' ' ')"
+COREDEPLIST="$(cat ~/.readme/core | grep '^#' | grep -v '^$' | cut --delimiter=' ' --field='2' | tr '\n' ' ')"
+
 GROUPSLIST="$(cat ~/.readme/groups | tr '\n' ' ')"
 
-PKGLIST="$GROUPSLIST $CORELIST"
+echo "---core---"
+echo "$CORELIST"
+echo ""
 
-echo "---core/group---"
-echo "$PKGLIST"
-sudo -k  pacman -S $PKGLIST --needed
+echo "---coredep---"
+echo "$COREDEPLIST"
+echo ""
+
+echo "---groups---"
+echo "$GROUPSLIST"
+echo ""
 
 #-----------------------------------------------------------------------
 
-git -C ~/Packages/pkgaur/ pull
-AURLIST="$(cat ~/.readme/aur)"
-AURVERSIONLIST=""
-
-while read -r line
-do
-	AURVERSIONLIST+="$(find ~/Packages/pkgaur/core -name "$line*") "
-done <<< "$AURLIST"
+AURLIST="$(cat ~/.readme/aur | grep -v '^#' | grep -v '^$')"
+AURDEPLIST="$(cat ~/.readme/aur | grep '^#' | grep -v '^$' | cut --delimiter=' ' --field='2')"
 
 echo "---aur---"
-echo "$(echo $AURLIST | sed 's/\n/ /g')"
-sudo -k pacman -U $AURVERSIONLIST --needed
+echo "$(echo $AURLIST | tr '\n' ' ')"
+echo ""
+
+echo "---aurdep---"
+echo "$(echo $AURDEPLIST | tr '\n' ' ')"
+echo ""
+
+AURVERSIONLIST=""
+while read line ; do
+	AURVERSIONLIST+="$(find ~/Packages/pkgaur/core -name "$line*") " || exit 10
+done <<< "$AURLIST"
+
+AURDEPVERSIONLIST=""
+while read line ; do
+	AURDEPVERSIONLIST+="$(find ~/Packages/pkgaur/core -name "$line*") " || exit 11
+done <<< "$AURDEPLIST"
+
+#-----------------------------------------------------------------------
+
+function callPacman {
+    echo "Calling pacman."
+
+    sudo --remove-timestamp
+
+    sudo pacman -Syu &&
+    sudo pacman -S $CORELIST --asexplicit --needed &&
+    sudo pacman -S $COREDEPLIST --asdeps --needed &&
+
+    #sudo pacman -S $GROUPSLIST --needed
+
+    sudo pacman -U $AURVERSIONLIST --needed &&
+    sudo pacman -U $AURDEPVERSIONLIST --needed &&
+
+    echo "Called pacman succesfully."
+
+    return 0
+}
+
+echo "Syncing 'pkgaur'-Repository."
+git -C ~/Packages/pkgaur/ pull
+
+callPacman || exit 1
