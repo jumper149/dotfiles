6e186f584cdd101a2887ea684020c78f39367b96
Felix Springer <felixspringer149@gmail.com>
2019-10-30 02:18:40 +0100

    updated system-info for jumpnix

--- .system-info.sh
+++ .system-info.sh
@@ -6,12 +6,12 @@
 # main webcam
 # main touchpad
 
-if [ "$(hostname)" = "x220arch" ]
+if [ "$(hostname)" = "jumpnix" ]
 then
 	MAIN_NETWORKING_DEVICE='wlp2s0'
-	MAIN_PARTITION='/dev/sda3'
-	MAIN_PULSEAUDIO_SINK='0'
-	MAIN_PULSEAUDIO_SOURCE='1'
+	MAIN_PARTITION='/dev/sda1'
+	MAIN_PULSEAUDIO_SINK='alsa_output.pci-0000_00_1b.0.analog-stereo'
+	MAIN_PULSEAUDIO_SOURCE='alsa_output.pci-0000_00_1b.0.analog-stereo.monitor'
 	MAIN_WEBCAM='/dev/video0'
 	MAIN_TOUCHPAD='SynPS/2 Synaptics TouchPad'
 elif [ "$(hostname)" = "x201arch" ]
