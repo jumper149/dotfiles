8f8d9f40c8b79a378b5c8e9f20d8690e79f2807a
Felix Springer <felixspringer149@gmail.com>
2021-11-28 19:34:57 +0100

    Update nixremote -> nixbase links for qutebrowser.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -37,11 +37,11 @@ config.bind('<Ctrl-r>', 'jseval -w main DarkReader.disable()')
 
 
 
-c.url.default_page = "http://nixremote.wireguard/searx/"
-c.url.start_pages = "http://nixremote.wireguard/searx/"
+c.url.default_page = "http://nixbase.wireguard/searx/"
+c.url.start_pages = "http://nixbase.wireguard/searx/"
 
-c.url.searchengines['DEFAULT'] = "http://nixremote.wireguard/searx/search?q={}"
-c.url.searchengines['s'] = "http://nixremote.wireguard/searx/search?q={}"
+c.url.searchengines['DEFAULT'] = "http://nixbase.wireguard/searx/search?q={}"
+c.url.searchengines['s'] = "http://nixbase.wireguard/searx/search?q={}"
 c.url.searchengines['d'] = "https://duckduckgo.com/?q={}"
 c.url.searchengines['g'] = "https://www.google.com/search?hl=en&q={}"
 c.url.searchengines['w'] = "https://de.wikipedia.org/w/index.php?search={}"
