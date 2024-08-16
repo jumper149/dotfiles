567fac81910a56b8b5265d2f3621ba768d6a10e3
Felix Springer <felixspringer149@gmail.com>
2024-08-16 10:12:19 +0200

    Just print from blucontrol for fun.

--- .config/blucontrol/blucontrol.hs
+++ .config/blucontrol/blucontrol.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Control.Monad (void)
+import Data.Word
 
 import Blucontrol
-import Blucontrol.Monad.ApplyValue.X
+import Blucontrol.Monad.ApplyValue.Print
 import Blucontrol.Monad.Control.Count
 import Blucontrol.Monad.Control.Wait
 import Blucontrol.Monad.PrepareValue.Linear
+import Blucontrol.Value.RGB
 import Blucontrol.Value.RGB.Temperature
 
 main :: IO ()
 main = void $ blucontrol configControl
   where configControl = ConfigControl { runControl = runControlCountT def !> runControlWaitT configWait
                                       , runPrepareValue = runPrepareValueLinearT @Temperature rgbMap
-                                      , runApplyValue = runApplyValueXTIO def
+                                      , runApplyValue = runApplyValuePrintT @(RGB Word8)
                                       }
         rgbMap = 13:.00 ==> 7000
             :| [ 17:.00 ==> 5000
